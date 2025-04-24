2f29e2d043a274b4744210de00f76b0215cb85f1
박민형 <149697705+par229@users.noreply.github.com>
2025-04-24 17:29:04 +0900

    API key remove

--- memo_backend/core/admin.py
+++ memo_backend/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Board, Memo  # ✅ 너의 모델들 import
+from .models import Board, Memo 
 
-admin.site.register(Board)  # ✅ 등록
-admin.site.register(Memo)   # ✅ 등록
+admin.site.register(Board)
+admin.site.register(Memo) 
---
+++ memo_backend/memo_backend/settings.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+import os
+
+OPENAI_API_KEY = "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"
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-v%elj5h(l*p_6!y6vk!e+^!s=cv$ty8mdbrce$=@j-(wzd$wtn'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin', 
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+    'core',
+    'django_filters',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+ROOT_URLCONF = 'memo_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'memo_backend.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'memo_db',
+        'USER': 'memore',
+        'PASSWORD': 'als229',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+AUTH_USER_MODEL = 'core.User'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Seoul'
+USE_TZ = False
+USE_I18N = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
